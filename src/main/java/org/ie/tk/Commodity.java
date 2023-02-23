5ccb8c334274d844e7ab3007cef18bf3496976ce
Kianoush <kianoosharshi@gmail.com>
2023-02-23 22:51:00 +0330

    Add User class and functionality. Add Commodity Class and partial functionality.

--- src/main/java/org/ie/tk/Commodity.java
+++ src/main/java/org/ie/tk/Commodity.java
@@ -1,4 +1,66 @@
 package org.ie.tk;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class Commodity {
+    private String id;
+    private String name;
+    private String providerId;
+    private Double price;
+    private ArrayList<String> categories;
+    private Double rating;
+    private Integer inStock;
+    private final HashMap<String, Double> userRatings = new HashMap<>();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Commodity other) {
+            return Objects.equals(this.id, other.id);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Double getRating() {
+        Double sum = rating;
+        for (Double userRating : userRatings.values()) {
+            sum += userRating;
+        }
+        return sum / (userRatings.size() + 1);
+    }
+
+    public ArrayNode getCategories() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayNode categoriesNode = objectMapper.createArrayNode();
+        for (String category: categories) {
+            categoriesNode.add(category);
+        }
+        return categoriesNode;
+    }
 }
--- src/main/java/org/ie/tk/User.java
+++ src/main/java/org/ie/tk/User.java
@@ -1,4 +1,57 @@
 package org.ie.tk;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.ie.tk.Exception.CommodityInBuyList;
+import org.ie.tk.Exception.CommodityNotFound;
+
+import java.util.ArrayList;
+
 public class User {
+    private String username;
+    private String email;
+    private String password;
+    private String birthDate;
+    private String address;
+    private Integer credit;
+    private final ArrayList<Commodity> buyList = new ArrayList<>();
+
+    public void addToBuyList(Commodity commodity) throws CommodityInBuyList {
+        if (buyList.contains(commodity)) {
+            throw new CommodityInBuyList(this.username, commodity.getId());
+        }
+        buyList.add(commodity);
+        //TODO
+        // Decrease me there in system
+        // Check found and inStock in system
+    }
+
+    public void removeFromBuyList(Commodity commodity) throws CommodityNotFound {
+        if (!buyList.contains(commodity)){
+            throw new CommodityNotFound(commodity.getId());
+        }
+        buyList.remove(commodity);
+        //TODO
+        // Increase inStock
+    }
+
+    public ArrayNode getBuyList() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayNode buyListNode = objectMapper.createArrayNode();
+
+        for (Commodity commodity : buyList) {
+            ObjectNode commodityNode = objectMapper.createObjectNode();
+            commodityNode.put("id", commodity.getId());
+            commodityNode.put("name", commodity.getName());
+            commodityNode.put("providerId", commodity.getProviderId());
+            commodityNode.put("price", commodity.getPrice());
+            commodityNode.set("categories", commodity.getCategories());
+            commodityNode.put("rating", commodity.getRating());
+            buyListNode.add(commodityNode);
+        }
+
+        return buyListNode;
+    }
+
 }
