765dac6c7d1ac4ad4985356b1f410e802f01c511
tahafkh <fakharian.taha@ut.ac.ir>
2023-02-24 22:46:51 +0330

    Add initial hashmaps

--- src/main/java/org/ie/tk/CommodityProvisionSystem.java
+++ src/main/java/org/ie/tk/CommodityProvisionSystem.java
@@ -1,4 +1,16 @@
 package org.ie.tk;
 
+import java.util.HashMap;
+
 public class CommodityProvisionSystem {
+
+    private HashMap<String, Commodity> commodities;
+    private HashMap<String, Provider> providers;
+    private HashMap<String, User> users;
+
+    public CommodityProvisionSystem() {
+        commodities = new HashMap<>();
+        providers = new HashMap<>();
+        users = new HashMap<>();
+    }
 }
