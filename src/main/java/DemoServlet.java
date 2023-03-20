ed6e8c04d06270638907f6a73fa2f215daf94f8d
noushkia <kianoosharshi@gmail.com>
2023-03-20 13:11:22 +0330

    Set up Tomcat.

---
+++ src/main/java/DemoServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/")
+public class DemoServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World!</h3>");
+    }
+}
