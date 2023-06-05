861a46d639ea5bc0cee378daaa35036eabe9efa4
tahafkh <fakharian.taha@ut.ac.ir>
2023-06-05 13:56:42 +0330

    Remove useless imports

--- src/main/java/com/oak/application/api/AuthFilter.java
+++ src/main/java/com/oak/application/api/AuthFilter.java
@@ -4,7 +4,6 @@ import com.oak.application.service.AuthService;
 import com.oak.exception.Auth.InvalidAuthorization;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +11,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
 import java.time.Instant;
 import java.util.*;
 
